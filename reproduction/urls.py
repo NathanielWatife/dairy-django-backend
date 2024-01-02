dbe3c7a65d20a0c47b18a6c7ab7015198b6b1211
Peter Evance <peterevance1@gmail.com>
2024-01-02 14:53:43 +0300

    Registered URL...

--- reproduction/urls.py
+++ reproduction/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from reproduction.views import PregnancyViewSet, HeatViewSet
+from reproduction.views import PregnancyViewSet, HeatViewSet, InseminationViewset
 
 app_name = 'reproduction'
 
 router = routers.DefaultRouter()
 router.register(r'pregnancy-records', PregnancyViewSet, basename='pregnancy-records')
 router.register(r'heat-records', HeatViewSet, basename='heat-records')
+router.register(r'insemination-records', InseminationViewset, basename='insemination-records')
 
 urlpatterns = [
     path('', include(router.urls))
