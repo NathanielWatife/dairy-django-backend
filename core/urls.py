2dc35fee64cff9b1b4e8d918ca4d7fd424e0da4f
Reagan <mamesareagan0@gmail.com>
2024-01-05 22:35:03 +0300

    feat: Add CowInventory serializer and viewset
    - Introduce CowInventorySerializer to handle serialization of CowInventory model.
    - Implement CowInventoryViewSet with read-only functionality.
    - Set permissions to allow access for farm owners and managers only.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from core.views import CowBreedViewSet, CowViewSet, InseminatorViewset
+from core.views import CowBreedViewSet, CowViewSet, InseminatorViewset, CowInventoryViewSet
 
 app_name = "core"
 
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 router.register(r"cow-breeds", CowBreedViewSet, basename="cow-breeds")
 router.register(r"cows", CowViewSet, basename="cows")
 router.register(r'inseminator-records', InseminatorViewset, basename='inseminator-records')
+router.register(r'cow-inventory', CowInventoryViewSet, basename='cow-inventory') 
 
 urlpatterns = [
     path("", include(router.urls)),
--- core/views.py
+++ core/views.py
@@ -4,8 +4,8 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
 from core.filters import CowBreedFilterSet, CowFilterSet, InseminatorFilterSet
-from core.models import Cow, CowBreed, Inseminator
-from core.serializers import CowBreedSerializer, CowSerializer, InseminatorSerializer
+from core.models import Cow, CowBreed, Inseminator, CowInventory
+from core.serializers import CowBreedSerializer, CowSerializer, InseminatorSerializer, CowInventorySerializer
 from users.permissions import (
     IsFarmOwner,
     IsFarmManager,
@@ -225,5 +225,46 @@ class InseminatorViewset(viewsets.ModelViewSet):
                 )
 
         serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CowInventoryViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet to handle operations related to cow inventory.
+
+    Provides read-only functionality for cow inventory.
+
+    Actions:
+    - list: Get the cow inventory.
+           Returns a 200 response with the cow inventory data.
+
+    Serializer class used for response data: CowInventorySerializer.
+
+    Permissions:
+    - Accessible to all authenticated users (farm workers, assistant farm managers, farm managers, farm owners).
+
+    """
+
+    queryset = CowInventory.objects.all()
+    serializer_class = CowInventorySerializer
+    permission_classes = [IsFarmManager | IsFarmOwner]
+
+    def list(self, request, *args, **kwargs):
+        """
+        Get the cow inventory.
+
+        Returns a 200 response with the cow inventory data.
+
+        """
+        cow_inventory = self.get_queryset().first()
+
+        if not cow_inventory:
+            # If there is no cow inventory in the database
+            return Response(
+                {"detail": "No cow inventory found in the farm yet."},
+                status=status.HTTP_200_OK,
+            )
+
+        serializer = self.get_serializer(cow_inventory)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
