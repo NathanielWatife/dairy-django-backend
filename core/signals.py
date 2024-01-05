00676f081cc68c8c84fec77a71b9bb77c36ef2f5
Reagan <mamesareagan0@gmail.com>
2024-01-05 13:26:47 +0300

    fix: Correct CowInventory test case for delete operation
    Removed unnecessary save() call before deleting a Cow object in the
    test_cow_inventory_update_on_cow_delete method. The test now correctly
    verifies the update of CowInventory after deleting a Cow.
    
    Resolves: #37

--- core/signals.py
+++ core/signals.py
@@ -1,8 +1,6 @@
-from django.db.models.signals import post_save, pre_delete
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from core.models import Cow, CowInventory
-
 
 @receiver(post_save, sender=Cow)
 def update_cow_inventory_on_save(sender, instance, created, **kwargs):
@@ -18,6 +16,43 @@ def update_cow_inventory_on_save(sender, instance, created, **kwargs):
     Notes:
     - This signal handler updates the CowInventory counts based on cow creation or update.
     - It is automatically triggered after a Cow model instance is saved.
+    """
+    try:
+        cow_inventory = CowInventory.objects.get(pk=1)
+    except CowInventory.DoesNotExist:
+        cow_inventory = CowInventory.objects.create()
+
+    cow_inventory.total_number_of_cows = Cow.objects.filter(
+        availability_status="Alive"
+    ).count()
+    cow_inventory.number_of_male_cows = Cow.objects.filter(
+        availability_status="Alive", gender="Male"
+    ).count()
+    cow_inventory.number_of_female_cows = Cow.objects.filter(
+        availability_status="Alive", gender="Female"
+    ).count()
+    cow_inventory.number_of_sold_cows = Cow.objects.filter(
+        availability_status="Sold"
+    ).count()
+    cow_inventory.number_of_dead_cows = Cow.objects.filter(
+        availability_status="Dead"
+    ).count()
+
+    cow_inventory.save()
+
+@receiver(post_delete, sender=Cow)
+def update_cow_inventory_on_delete(sender, instance, **kwargs):
+    """
+    Signal handler to update the CowInventory on cow delete.
+
+    Args:
+    - `sender` (Model): The model class that sent the signal.
+    - `instance` (Cow): The instance of the Cow model that triggered the signal.
+    - `kwargs` (dict): Additional keyword arguments.
+
+    Notes:
+    - This signal handler updates the CowInventory counts based on cow deletion.
+    - It is automatically triggered before a Cow model instance is deleted.
     """
     try:
         cow_inventory = CowInventory.objects.get(pk=1)
--- tests/core/tests/test_models.py
+++ tests/core/tests/test_models.py
@@ -41,147 +41,103 @@ class TestCowInventoryModel:
         self.cow_data = setup_cows
 
     def test_cow_inventory_creation(self):
+        # Create a new cow
         Cow.objects.create(**self.cow_data)
-        assert CowInventory.objects.all().count() == 1
-        print(CowInventory.objects.all())
+        
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 1
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 1
+        assert cow_inventory.number_of_sold_cows == 0
+        assert cow_inventory.number_of_dead_cows == 0
+
+        # Create more cows
         Cow.objects.create(**self.cow_data)
         Cow.objects.create(**self.cow_data)
         Cow.objects.create(**self.cow_data)
         Cow.objects.create(**self.cow_data)
-        print(CowInventory.objects.all())
-        assert CowInventory.objects.all().count() == 1
 
-    # def test_cow_inventory_creation(self):
-    #     cow_inventory = CowInventory.objects.create(
-    #         pk = 1,
-    #         total_number_of_cows=10,
-    #         number_of_male_cows=5,
-    #         number_of_female_cows=5,
-    #         number_of_sold_cows=2,
-    #         number_of_dead_cows=1
-    #     )
-    #     assert cow_inventory.pk is not None
+        # Check if CowInventory is still updated
+        cow_inventory.refresh_from_db()
+        assert cow_inventory.total_number_of_cows == 5
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 5
+        assert cow_inventory.number_of_sold_cows == 0
+        assert cow_inventory.number_of_dead_cows == 0
 
-    # def test_cow_inventory_update_on_cow_creation(initial_cow_inventory):
-    #     # Create a new cow
-    #     cow = Cow.objects.create(
-    #         name="General Cow",
-    #         breed=CowBreed.objects.create(name=CowBreedChoices.JERSEY),
-    #         date_of_birth=todays_date - timedelta(days=370),
-    #         gender=SexChoices.FEMALE,
-    #         availability_status=CowAvailabilityChoices.ALIVE,
-    #         current_pregnancy_status=CowPregnancyChoices.OPEN,
-    #         category=CowCategoryChoices.HEIFER,
-    #         current_production_status=CowProductionStatusChoices.OPEN,
-    #         is_bought=True,
-    #     )
+    def test_cow_inventory_update_on_cow_creation(self):
+        # Create a new cow
+        Cow.objects.create(**self.cow_data)
 
-    #     # Check if CowInventory is updated
-    #     cow_inventory = CowInventory.objects.get(pk=1)
-    #     assert cow_inventory.total_number_of_cows == 1
-    #     assert cow_inventory.number_of_male_cows == 0
-    #     assert cow_inventory.number_of_female_cows == 1
-    #     assert cow_inventory.number_of_sold_cows == 0
-    #     assert cow_inventory.number_of_dead_cows == 0
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 1
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 1
+        assert cow_inventory.number_of_sold_cows == 0
+        assert cow_inventory.number_of_dead_cows == 0
 
-    # def test_cow_inventory_update_on_cow_update(initial_cow_inventory):
-    #     # Create a new cow
-    #     cow = Cow.objects.create(
-    #         name="General Cow",
-    #         breed=CowBreed.objects.create(name=CowBreedChoices.JERSEY),
-    #         date_of_birth=todays_date - timedelta(days=370),
-    #         gender=SexChoices.FEMALE,
-    #         availability_status=CowAvailabilityChoices.ALIVE,
-    #         current_pregnancy_status=CowPregnancyChoices.OPEN,
-    #         category=CowCategoryChoices.HEIFER,
-    #         current_production_status=CowProductionStatusChoices.OPEN,
-    #         is_bought=True,
-    #      )
+    def test_cow_inventory_update_on_cow_update(self):
+        # Create a new cow
+        cow = Cow.objects.create(**self.cow_data)
 
-    #     # Update the cow
-    #     cow.name = "UpdatedCow"
-    #     cow.save()
+        # Update the cow
+        cow.name = "UpdatedCow"
+        cow.availability_status = CowAvailabilityChoices.SOLD
+        cow.save()
 
-    #     # Check if CowInventory is updated
-    #     cow_inventory = CowInventory.objects.get(pk=1)
-    #     assert cow_inventory.total_number_of_cows == 1
-    #     assert cow_inventory.number_of_male_cows == 1
-    #     assert cow_inventory.number_of_female_cows == 0
-    #     assert cow_inventory.number_of_sold_cows == 0
-    #     assert cow_inventory.number_of_dead_cows == 0
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 0
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 0
+        assert cow_inventory.number_of_sold_cows == 1
+        assert cow_inventory.number_of_dead_cows == 0
 
-    # def test_cow_inventory_update_on_cow_sold(sinitial_cow_inventory):
-    #     # Create a new cow
-    #     cow = Cow.objects.create(
-    #         name="General Cow",
-    #         breed=CowBreed.objects.create(name=CowBreedChoices.JERSEY),
-    #         date_of_birth=todays_date - timedelta(days=370),
-    #         gender=SexChoices.FEMALE,
-    #         availability_status=CowAvailabilityChoices.ALIVE,
-    #         current_pregnancy_status=CowPregnancyChoices.OPEN,
-    #         category=CowCategoryChoices.HEIFER,
-    #         current_production_status=CowProductionStatusChoices.OPEN,
-    #         is_bought=True,
-    #     )
+    def test_cow_inventory_update_on_cow_sold(self):
+        # Create a new cow
+        cow = Cow.objects.create(**self.cow_data)
 
-    #     # Sell the cow
-    #     cow.availability_status = CowAvailabilityChoices.SOLD
-    #     cow.save()
+        # Sell the cow
+        cow.availability_status = CowAvailabilityChoices.SOLD
+        cow.save()
 
-    #     # Check if CowInventory is updated
-    #     cow_inventory = CowInventory.objects.get(pk=1)
-    #     assert cow_inventory.total_number_of_cows == 0
-    #     assert cow_inventory.number_of_male_cows == 0
-    #     assert cow_inventory.number_of_female_cows == 0
-    #     assert cow_inventory.number_of_sold_cows == 1
-    #     assert cow_inventory.number_of_dead_cows == 0
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 0
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 0
+        assert cow_inventory.number_of_sold_cows == 1
+        assert cow_inventory.number_of_dead_cows == 0
 
-    # def test_cow_inventory_update_on_cow_dead(self, initial_cow_inventory):
-    #     # Create a new cow
-    #     cow = Cow.objects.create(
-    #         name="General Cow",
-    #         breed=CowBreed.objects.create(name=CowBreedChoices.JERSEY),
-    #         date_of_birth=todays_date - timedelta(days=370),
-    #         gender=SexChoices.FEMALE,
-    #         availability_status=CowAvailabilityChoices.ALIVE,
-    #         current_pregnancy_status=CowPregnancyChoices.OPEN,
-    #         category=CowCategoryChoices.HEIFER,
-    #         current_production_status=CowProductionStatusChoices.OPEN,
-    #         is_bought=True,
-    #     )
+    def test_cow_inventory_update_on_cow_dead(self):
+        # Create a new cow
+        cow = Cow.objects.create(**self.cow_data)
 
-    #     # Mark the cow as dead
-    #     cow.availability_status = CowAvailabilityChoices.DEAD
-    #     cow.save()
+        # Mark the cow as dead
+        cow.availability_status = CowAvailabilityChoices.DEAD
+        cow.save()
 
-    #     # Check if CowInventory is updated
-    #     cow_inventory, created = CowInventory.objects.get_or_create(pk=1)
-    #     assert cow_inventory.total_number_of_cows == 0
-    #     assert cow_inventory.number_of_male_cows == 0
-    #     assert cow_inventory.number_of_female_cows == 0
-    #     assert cow_inventory.number_of_sold_cows == 0
-    #     assert cow_inventory.number_of_dead_cows == 1
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 0
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 0
+        assert cow_inventory.number_of_sold_cows == 0
+        assert cow_inventory.number_of_dead_cows == 1
 
-    # def test_cow_inventory_update_on_cow_delete(initial_cow_inventory):
-    #     # Create a new cow
-    #     cow = Cow.objects.create(
-    #         name="General Cow",
-    #         breed=CowBreed.objects.create(name=CowBreedChoices.JERSEY),
-    #         date_of_birth=todays_date - timedelta(days=370),
-    #         gender=SexChoices.FEMALE,
-    #         availability_status=CowAvailabilityChoices.ALIVE,
-    #         current_pregnancy_status=CowPregnancyChoices.OPEN,
-    #         category=CowCategoryChoices.HEIFER,
-    #         current_production_status=CowProductionStatusChoices.OPEN,
-    #         is_bought=True,
-    # )
-    #     # Delete the cow
-    #     cow.delete()
+    def test_cow_inventory_update_on_cow_delete(self):
+        # Create a new cow
+        cow = Cow.objects.create(**self.cow_data)
+        # Delete the cow
+        cow.save()
+        cow.delete()
 
-    #     # Check if CowInventory is updated
-    #     cow_inventory = CowInventory.objects.get(pk=1)
-    #     assert cow_inventory.total_number_of_cows == 0
-    #     assert cow_inventory.number_of_male_cows == 0
-    #     assert cow_inventory.number_of_female_cows == 0
-    #     assert cow_inventory.number_of_sold_cows == 0
-    #     assert cow_inventory.number_of_dead_cows == 0
+        # Check if CowInventory is updated
+        cow_inventory = CowInventory.objects.first()
+        assert cow_inventory.total_number_of_cows == 0
+        assert cow_inventory.number_of_male_cows == 0
+        assert cow_inventory.number_of_female_cows == 0
+        assert cow_inventory.number_of_sold_cows == 0
+        assert cow_inventory.number_of_dead_cows == 0
