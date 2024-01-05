761ea926d21602dadd881878ea5f494ee80a20f7
Reagan <mamesareagan0@gmail.com>
2024-01-05 13:42:13 +0300

    Removed unnecessary save() call before deleting a Cow object in the test_cow_inventory_update_on_cow_delete method. The test now correctly verifies the update of CowInventory after deleting a Cow.
    Resolves: #37

--- tests/core/tests/test_models.py
+++ tests/core/tests/test_models.py
@@ -13,25 +13,25 @@ from users.choices import SexChoices
 from core.utils import todays_date
 from datetime import timedelta
 
-# @pytest.mark.django_db
-# class TestCowBreedModel:
-#     def test_save_breed_with_valid_name(self):
-#         breed = CowBreed.objects.create(name=CowBreedChoices.JERSEY)
-#         assert breed.name == CowBreedChoices.JERSEY
+@pytest.mark.django_db
+class TestCowBreedModel:
+    def test_save_breed_with_valid_name(self):
+        breed = CowBreed.objects.create(name=CowBreedChoices.JERSEY)
+        assert breed.name == CowBreedChoices.JERSEY
 
-#     def test_create_breed_with_invalid_name(self):
-#         with pytest.raises(ValidationError) as err:
-#             CowBreed.objects.create(name="unknown_breed")
-#         assert err.value.code == 'invalid_cow_breed'
+    def test_create_breed_with_invalid_name(self):
+         with pytest.raises(ValidationError) as err:
+             CowBreed.objects.create(name="unknown_breed")
+         assert err.value.code == 'invalid_cow_breed'
 
-#     def test_create_breed_with_duplicate_name(self):
-#         # Create a breed with a valid name first
-#         CowBreed.objects.create(name=CowBreedChoices.FRIESIAN)
+    def test_create_breed_with_duplicate_name(self):
+         # Create a breed with a valid name first
+         CowBreed.objects.create(name=CowBreedChoices.FRIESIAN)
 
-#         # Attempt to create another breed with the same name, should raise ValidationError
-#         with pytest.raises(ValidationError) as err:
-#             CowBreed.objects.create(name=CowBreedChoices.FRIESIAN)
-#         assert err.value.code == 'duplicate_cow_breed'
+         # Attempt to create another breed with the same name, should raise ValidationError
+         with pytest.raises(ValidationError) as err:
+             CowBreed.objects.create(name=CowBreedChoices.FRIESIAN)
+         assert err.value.code == 'duplicate_cow_breed'
 
 
 @pytest.mark.django_db
