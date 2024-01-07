6aa58ae54311587bb2248cf834a19b6210022509
Peter Evance <peterevance1@gmail.com>
2024-01-07 14:58:36 +0300

    Added pathogen testcases

--- tests/health/tests/test_views.py
+++ tests/health/tests/test_views.py
@@ -292,3 +292,88 @@ class TestQuarantineRecordViewSet:
             HTTP_AUTHORIZATION=f"Token {self.tokens[user_type]}",
         )
         assert response.status_code == expected_status
+
+
+@pytest.mark.django_db
+class TestPathogenViewSet:
+    @pytest.fixture(autouse=True)
+    def setup(self, setup_users):
+        self.client = setup_users["client"]
+        self.tokens = {
+            "farm_owner": setup_users["farm_owner_token"],
+            "farm_manager": setup_users["farm_manager_token"],
+            "asst_farm_manager": setup_users["asst_farm_manager_token"],
+            "team_leader": setup_users["team_leader_token"],
+            "farm_worker": setup_users["farm_worker_token"],
+        }
+
+    @pytest.mark.parametrize(
+        "user_type, expected_status",
+        [
+            ("farm_owner", status.HTTP_201_CREATED),
+            ("farm_manager", status.HTTP_201_CREATED),
+            ("asst_farm_manager", status.HTTP_403_FORBIDDEN),
+            ("farm_worker", status.HTTP_403_FORBIDDEN),
+        ],
+    )
+    def test_create_pathogen(self, user_type, expected_status):
+        pathogen_data = {"name": PathogenChoices.BACTERIA}
+        response = self.client.post(
+            reverse("health:pathogens-list"),
+            pathogen_data,
+            HTTP_AUTHORIZATION=f"Token {self.tokens[user_type]}",
+        )
+        assert response.status_code == expected_status
+
+    @pytest.mark.parametrize(
+        "user_type, expected_status",
+        [
+            ("farm_owner", status.HTTP_200_OK),
+            ("farm_manager", status.HTTP_200_OK),
+            ("asst_farm_manager", status.HTTP_403_FORBIDDEN),
+            ("farm_worker", status.HTTP_403_FORBIDDEN),
+        ],
+    )
+    def test_retrieve_pathogen(self, user_type, expected_status):
+        response = self.client.get(
+            reverse("health:pathogens-list"),
+            HTTP_AUTHORIZATION=f"Token {self.tokens[user_type]}",
+        )
+        assert response.status_code == expected_status
+
+    @pytest.mark.parametrize(
+        "user_type, expected_status",
+        [
+            ("farm_owner", status.HTTP_200_OK),
+            ("farm_manager", status.HTTP_200_OK),
+            ("asst_farm_manager", status.HTTP_403_FORBIDDEN),
+            ("farm_worker", status.HTTP_403_FORBIDDEN),
+        ],
+    )
+    def test_update_pathogen(self, user_type, expected_status):
+        pathogen = Pathogen.objects.create(name=PathogenChoices.UNKNOWN)
+        url = reverse("health:pathogens-detail", kwargs={"pk": pathogen.id})
+        pathogen_update_data = {"name": PathogenChoices.FUNGI}
+        response = self.client.put(
+            url,
+            pathogen_update_data,
+            HTTP_AUTHORIZATION=f"Token {self.tokens[user_type]}",
+        )
+        assert response.status_code == expected_status
+
+    @pytest.mark.parametrize(
+        "user_type, expected_status",
+        [
+            ("farm_owner", status.HTTP_204_NO_CONTENT),
+            ("farm_manager", status.HTTP_204_NO_CONTENT),
+            ("asst_farm_manager", status.HTTP_403_FORBIDDEN),
+            ("farm_worker", status.HTTP_403_FORBIDDEN),
+        ],
+    )
+    def test_delete_pathogen(self, user_type, expected_status):
+        pathogen = Pathogen.objects.create(name=PathogenChoices.UNKNOWN)
+        url = reverse("health:pathogens-detail", kwargs={"pk": pathogen.id})
+        response = self.client.delete(
+            url, HTTP_AUTHORIZATION=f"Token {self.tokens[user_type]}"
+        )
+        assert response.status_code == expected_status
