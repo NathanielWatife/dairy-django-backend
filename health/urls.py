d08aa448fc68a5523b10f7f7132f0ad59bb9ff3c
Peter Evance <peterevance1@gmail.com>
2024-01-07 14:57:25 +0300

    Registerd the pathogen URL

--- health/urls.py
+++ health/urls.py
@@ -2,7 +2,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from health.views import WeightRecordViewSet, CullingRecordViewSet, QuarantineRecordViewSet
+from health.views import (
+    WeightRecordViewSet,
+    CullingRecordViewSet,
+    QuarantineRecordViewSet,
+    PathogenViewSet,
+)
 
 app_name = 'health'
 
@@ -10,6 +15,7 @@ router = routers.DefaultRouter()
 router.register(r'weight-records', WeightRecordViewSet, basename='weight-records')
 router.register(r'culling-records', CullingRecordViewSet, basename='culling-records')
 router.register(r'quarantine-records', QuarantineRecordViewSet, basename='quarantine-records')
+router.register(r'pathogens', PathogenViewSet, basename='pathogens')
 
 urlpatterns = [
     path('', include(router.urls))
