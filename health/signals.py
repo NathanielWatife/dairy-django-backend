1b8d48d1805a447ae61fbc466e9fe29efbb5956b
Peter Evance <peterevance1@gmail.com>
2024-01-13 23:29:31 +0300

    Added `set_disease_recovery_record` signal for automatic creation of recovery records

--- health/signals.py
+++ health/signals.py
@@ -1,8 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 
 from core.choices import CowProductionStatusChoices, CowPregnancyChoices
-from health.models import CullingRecord, QuarantineRecord
+from core.models import Cow
+from health.models import CullingRecord, QuarantineRecord, Disease, Recovery
 
 
 @receiver(post_save, sender=CullingRecord)
@@ -29,6 +30,7 @@ def set_cow_production_status_to_culled(sender, instance, **kwargs):
         cow.current_pregnancy_status = CowPregnancyChoices.UNAVAILABLE
         cow.save()
 
+
 @receiver(post_save, sender=QuarantineRecord)
 def set_cow_availability_to_quarantined(sender, instance, **kwargs):
     """
@@ -50,3 +52,42 @@ def set_cow_availability_to_quarantined(sender, instance, **kwargs):
     if cow.availability_status != CowProductionStatusChoices.QUARANTINED:
         cow.availability_status = CowProductionStatusChoices.QUARANTINED
         cow.save()
+
+
+@receiver(m2m_changed, sender=Disease.cows.through)
+def set_disease_recovery_record(sender, instance, action, reverse, pk_set, **kwargs):
+    """
+        Signal handler for associating recovery records with cows after a Disease instance is linked.
+    
+        This signal is triggered when the Many-to-Many relationship between Disease and Cow instances is changed.
+        Specifically, it responds to the 'post_add' action, occurring after linking Disease instances to cows.
+        For each cow linked to the disease, it creates a Recovery record, indicating the diagnosis date.
+    
+        Args:
+        - `sender`: The sender of the signal (Disease model's M2M relation with cows).
+        - `instance`: The Disease instance being linked.
+        - `action`: The type of action triggering the signal (e.g., 'post_add').
+        - `reverse`: A boolean indicating whether the action is performed on the reverse side of the relation.
+        - `pk_set`: A set of primary keys of the related objects being added.
+        - `kwargs`: Additional keyword arguments passed to the signal handler.
+    
+        Usage:
+            This signal ensures that after linking Disease instances to cows, recovery records are created for each
+            cow, indicating the diagnosis date.
+    
+        Example:
+            To link a Disease instance to a set of cows:
+    
+            ```python
+            disease_instance.cows.add(cow1, cow2, cow3)
+            ```
+    
+            After this operation, the signal creates Recovery records for each cow.
+    
+        Note: This signal assumes that the 'date_reported' attribute of the Disease instance represents the diagnosis date.
+        """
+    if action == 'post_add' and not reverse:
+        for cow_id in pk_set:
+            cow = Cow.objects.get(pk=cow_id)
+            Recovery.objects.create(cow=cow, disease=instance, diagnosis_date=instance.date_reported)
+
