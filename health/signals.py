39a2cbbebefa76e09341bffd52a819d8d133ff73
Peter Evance <peterevance1@gmail.com>
2024-01-14 20:21:01 +0300

    Added signal `mark_cow_as_recovered`

--- health/signals.py
+++ health/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 
 from core.choices import CowProductionStatusChoices, CowPregnancyChoices
 from core.models import Cow
-from health.models import CullingRecord, QuarantineRecord, Disease, Recovery
+from health.models import CullingRecord, QuarantineRecord, Disease, Recovery, Treatment
 
 
 @receiver(post_save, sender=CullingRecord)
@@ -57,37 +57,61 @@ def set_cow_availability_to_quarantined(sender, instance, **kwargs):
 @receiver(m2m_changed, sender=Disease.cows.through)
 def set_disease_recovery_record(sender, instance, action, reverse, pk_set, **kwargs):
     """
-        Signal handler for associating recovery records with cows after a Disease instance is linked.
-    
-        This signal is triggered when the Many-to-Many relationship between Disease and Cow instances is changed.
-        Specifically, it responds to the 'post_add' action, occurring after linking Disease instances to cows.
-        For each cow linked to the disease, it creates a Recovery record, indicating the diagnosis date.
-    
-        Args:
-        - `sender`: The sender of the signal (Disease model's M2M relation with cows).
-        - `instance`: The Disease instance being linked.
-        - `action`: The type of action triggering the signal (e.g., 'post_add').
-        - `reverse`: A boolean indicating whether the action is performed on the reverse side of the relation.
-        - `pk_set`: A set of primary keys of the related objects being added.
-        - `kwargs`: Additional keyword arguments passed to the signal handler.
-    
-        Usage:
-            This signal ensures that after linking Disease instances to cows, recovery records are created for each
-            cow, indicating the diagnosis date.
-    
-        Example:
-            To link a Disease instance to a set of cows:
-    
-            ```python
-            disease_instance.cows.add(cow1, cow2, cow3)
-            ```
-    
-            After this operation, the signal creates Recovery records for each cow.
-    
-        Note: This signal assumes that the 'date_reported' attribute of the Disease instance represents the diagnosis date.
-        """
-    if action == 'post_add' and not reverse:
+    Signal handler for associating recovery records with cows after a Disease instance is linked.
+
+    This signal is triggered when the Many-to-Many relationship between Disease and Cow instances is changed.
+    Specifically, it responds to the 'post_add' action, occurring after linking Disease instances to cows.
+    For each cow linked to the disease, it creates a Recovery record, indicating the diagnosis date.
+
+    Args:
+    - `sender`: The sender of the signal (Disease model's M2M relation with cows).
+    - `instance`: The Disease instance being linked.
+    - `action`: The type of action triggering the signal (e.g., 'post_add').
+    - `reverse`: A boolean indicating whether the action is performed on the reverse side of the relation.
+    - `pk_set`: A set of primary keys of the related objects being added.
+    - `kwargs`: Additional keyword arguments passed to the signal handler.
+
+    Usage:
+        This signal ensures that after linking Disease instances to cows, recovery records are created for each
+        cow, indicating the diagnosis date.
+
+    Example:
+        To link a Disease instance to a set of cows:
+
+        ```python
+        disease_instance.cows.add(cow1, cow2, cow3)
+        ```
+
+        After this operation, the signal creates Recovery records for each cow.
+
+    Note: This signal assumes that the 'date_reported' attribute of the Disease instance represents the diagnosis date.
+    """
+    if action == "post_add" and not reverse:
         for cow_id in pk_set:
             cow = Cow.objects.get(pk=cow_id)
-            Recovery.objects.create(cow=cow, disease=instance, diagnosis_date=instance.date_reported)
+            Recovery.objects.create(
+                cow=cow, disease=instance, diagnosis_date=instance.date_reported
+            )
 
+
+@receiver(post_save, sender=Treatment)
+def mark_cow_as_recovered(sender, instance, **kwargs):
+    """
+    Signal handler for updating the recovery date of a cow after a Treatment instance is saved.
+
+    This signal is triggered after saving a Treatment instance. If the treatment is marked as 'completed' with a
+    specified completion date, it updates the corresponding Recovery record's recovery_date attribute.
+
+    Args:
+    - `sender`: The sender of the signal (Treatment model).
+    - `instance`: The Treatment instance being saved.
+    - `kwargs`: Additional keyword arguments passed to the signal handler.
+
+    Usage:
+        This signal ensures that after a Treatment instance is marked as 'completed' with a completion date,
+        the corresponding Recovery record's recovery date is updated.
+    """
+    if instance.completion_date:
+        recovery = Recovery.objects.get(cow=instance.cow, disease=instance.disease)
+        recovery.recovery_date = instance.completion_date
+        recovery.save()
